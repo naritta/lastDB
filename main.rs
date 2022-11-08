76aa81207f3f4e0fa488c85fe53172c0cc71d755
naritta <narittan@gmail.com>
2022-11-08 10:31:13 +0900

    Add set/get option

--- main.rs
+++ main.rs
@@ -64,17 +64,37 @@ fn main() {
     let mut buf = String::new();
 
     loop {
-        let mut key = String::new();
+        let mut input = String::new();
         io::stdin()
-            .read_line(&mut key)
+            .read_line(&mut input)
             .expect("failed");
-        print!("{}", key);
-        let mut rawkey = key.trim_end();
-        // let val = kv.get(&key);
-        match kv.get(rawkey) {
-            Some(review) => println!("{rawkey}: {review}"),
-            None => println!("{rawkey} is unreviewed.")
+        let mut split_input = input.split_whitespace();
+        // let mut type = split_input.next();
+        // let mut input = split_input.next();
+        // let mut rawkey = key.trim_end();
+        match split_input.next() {
+            Some("set") => match split_input.next() {
+                Some(key) => match split_input.next() {
+                    Some(val) => {
+                        kv.insert(String::from(key), String::from(val));
+                        println!("set {key}:{val}.");
+                    },
+                    None => println!("no value set.")
+                },
+                None => println!("unreviewed.")
+            },
+            Some("get") =>match split_input.next() {
+                Some(key) => match kv.get(key) {
+                    Some(val) => println!("got {val} for {key}"),
+                    None => println!("no value found fpr {key}.")
+                },
+                None => println!("unreviewed.")
+            },
+            Some(key) =>println!("unreviewed."),
+            None => println!("unreviewed.")
         }
+        // println!("input: {input}.")
+        // let val = kv.get(&key);
         // writeln!(writer, "{}", val);
     }
     // let r = io::stdin();
