fc7db688f09fd4113f72b895762bb5a718016d42
naritta <narittan@gmail.com>
2022-11-11 13:17:31 +0900

    Add server mode

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ extern crate yaml_rust;
 use yaml_rust::{YamlLoader, YamlEmitter};
 use std::collections::HashMap;
 use std::fs;
+use std::net::TcpListener;
 
 use std::io::{self, Read, Write, stdout};
 
@@ -13,13 +14,7 @@ fn load_yaml(path: &str) -> Vec<yaml_rust::Yaml> {
     docs
 }
 
-fn main() {
-    let path = "./src/main.yml";
-    let docs = load_yaml(&path);
-    let doc = &docs[0];
-
-    println!("MAX memory size:{}", doc["setting"][0]["max_memory"].as_i64().unwrap());
-
+fn local_run() {
     let mut kv = HashMap::new();
 
     kv.insert(String::from("a"), String::from("Yellow"));
@@ -62,6 +57,37 @@ fn main() {
         // let val = kv.get(&key);
         // writeln!(writer, "{}", val);
     }
+}
+
+fn server_run() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        println!("Connection established!");
+    }
+}
+
+fn main() {
+    let path = "./src/main.yml";
+    let docs = load_yaml(&path);
+    let doc = &docs[0];
+
+    println!("MAX memory size:{}", doc["setting"]["max_memory"].as_i64().unwrap());
+
+    match doc["setting"]["mode"].as_str() {
+        Some("local") => {
+            local_run();
+        },
+        Some("server") => {
+            server_run();
+        },
+        Some(key) =>println!("unreviewed."),
+        None => {
+            local_run();
+        }
+    }
+
     // let r = io::stdin();
     // let mut reader = r.lock();
     //
