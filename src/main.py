174cc44988566e7885acadb41eeb9f177b87287e
naritta <narittan@gmail.com>
2022-11-20 19:47:41 +0900

    Fix insert to my own slight better one

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+import bisect
+
 class BTreeNode:
     def __init__(self, leaf=False):
         self.leaf = leaf
@@ -13,57 +15,57 @@ class BTree:
     def init_refarr(self, arr):
         self.refarr = arr
 
-    # Insert node
-    def insert(self, k):
-        root = self.root
-        if len(root.keys) == (2 * self.t) - 1:
-            temp = BTreeNode()
-            self.root = temp
-            temp.child.insert(0, root)
-            self.split_child(temp, 0)
-            self.insert_non_full(temp, k)
+    def insert_node(self, val, node = None, MAX_NODE=2):
+        if not node:
+            node = self.root
+        i = bisect.bisect_right(node.keys, val)
+        # if it's full, try to insert in child of the node
+        if len(node.keys) == MAX_NODE:
+            if node.leaf:
+                midKey = node.keys[int(len(node.keys)/2)]
+                # TODO: fix to O(1)
+                node.keys.insert(i, val)
+                node.keys.remove(midKey)
+                # print(node.keys)
+                # TODO: perhaps should divide child also like when creating new root
+                return midKey
+            else:
+                retKey = self.insert_node(val, node.child[i])
+                midKey = node.keys[int(len(node.keys)/2)]
+                i = bisect.bisect_right(node.keys, retKey)
+                node.keys.insert(i, retKey)
+                node.keys.remove(midKey)
+                # print(node.keys)
+                # if it's root, create new root.
+                if node == self.root:
+                    newRoot = BTreeNode()
+                    newRoot.keys = [midKey]
+                    self.root = newRoot
+                    leftNode = BTreeNode()
+                    rightNode = BTreeNode()
+                    leftNode.keys.extend(node.keys[:int(len(node.keys)/2)])
+                    rightNode.keys.extend(node.keys[int(len(node.keys)/2):])
+                    leftNode.child.extend(node.child[:int(len(node.child)/2)])
+                    rightNode.child.extend(node.child[int(len(node.child)/2):])
+                    newRoot.child = [leftNode, rightNode]
+                else:
+                    return midKey
         else:
-            self.insert_non_full(root, k)
+            node.keys.insert(i, val)
 
-    # Insert nonfull
-    def insert_non_full(self, x, k):
-        i = len(x.keys) - 1
-        if x.leaf:
-            x.keys.append((None, None))
-            while i >= 0 and k[0] < x.keys[i][0]:
-                x.keys[i + 1] = x.keys[i]
-                i -= 1
-            x.keys[i + 1] = k
-        else:
-            while i >= 0 and k[0] < x.keys[i][0]:
-                i -= 1
-            i += 1
-            if len(x.child[i].keys) == (2 * self.t) - 1:
-                self.split_child(x, i)
-                if k[0] > x.keys[i][0]:
-                    i += 1
-            self.insert_non_full(x.child[i], k)
-
-    # Split the child
-    def split_child(self, x, i):
-        t = self.t
-        y = x.child[i]
-        z = BTreeNode(y.leaf)
-        x.child.insert(i + 1, z)
-        x.keys.insert(i, y.keys[t - 1])
-        z.keys = y.keys[t: (2 * t) - 1]
-        y.keys = y.keys[0: t - 1]
-        if not y.leaf:
-            z.child = y.child[t: 2 * t]
-            y.child = y.child[0: t - 1]
 
     def dump_tree(self):
-        que = [self.root]
+        que = [[self.root]]
         while que:
-            node = que.pop(0)
-            print(node.keys)
-            for childNode in node.child:
-                que.append(childNode)
+            nodes = que.pop(0)
+            print([node.keys for node in nodes])
+
+            for node in nodes:
+                nodes = []
+                for childNode in node.child:
+                    nodes.append(childNode)
+                if nodes:
+                    que.append(nodes)
 
     def initialize(self, L=0, R=15, D=3):
         if R-L > D:
@@ -87,6 +89,8 @@ def main():
     btree = BTree()
     btree.init_refarr([1, 2, 4, 6, 8, 10, 12, 13, 15, 16, 17, 18, 20, 22, 25])
     btree.root = btree.initialize()
+    # btree.dump_tree()
+    btree.insert_node(15)
     btree.dump_tree()
 
 if __name__ == "__main__":
