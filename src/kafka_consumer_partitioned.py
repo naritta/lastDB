3941c58f5224bfb778a99fb5a110e02d47994ffb
naritta <narittan@gmail.com>
2022-11-26 16:00:40 +0900

    Add partition for key type

--- src/kafka_consumer_partitioned.py
+++ src/kafka_consumer_partitioned.py
@@ -3,7 +3,8 @@ from kafka import TopicPartition
 
 def main():
     consumer = KafkaConsumer(bootstrap_servers='localhost:29092')
-    consumer.assign([TopicPartition('key', 0)])
+    consumer.assign([TopicPartition('key', 1)])
+    # consumer.subscribe(['key'])
     for msg in consumer:
       print (msg)
 
--- src/kafka_producer.py
+++ src/kafka_producer.py
@@ -2,9 +2,8 @@ from kafka import KafkaProducer
 
 def main():
     producer = KafkaProducer(bootstrap_servers='localhost:29092')
-    for _ in range(100):
-        producer.send('key', b'test_key')
-    future = producer.send('table', b'table_name')
+    future = producer.send('key', value=b'table_name', partition=0)
+    # future = producer.send('key', value=b'0', partition=1)
     result = future.get(timeout=60)
 
 if __name__ == "__main__":
